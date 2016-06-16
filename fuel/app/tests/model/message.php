bd55951a9fd4830a6214d499aa199e518efa1c00
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-06-16 17:29:28 +0000

    DBUnitで設定する初期テストデータに合わせて既存のテストを修正

--- fuel/app/tests/model/message.php
+++ fuel/app/tests/model/message.php
@@ -21,28 +21,28 @@ class Model_MessageTest extends PHPUnit_Extensions_Database_TestCase
         $this->assertEquals(1,1);
     }
 
-    #public function testログインしているユーザが発言したメッセージを降順で取得する()
-    #{
-    #    $result = Model_Message::message_find_by_userId_desc_by_createdAt(1);
-    #    $expect_userId = $result[3]->user_id;
-    #    $expect_desc = $result[4] > $result[3];
-    #    $this->assertEquals($expect_userId,1);
-    #    $this->assertEquals($expect_desc,true);
-    #}
+    public function testログインしているユーザが発言したメッセージを降順で取得する()
+    {
+        $result = Model_Message::message_find_by_userId_desc_by_createdAt(1);
+        $expect_userId = $result[1]->user_id;
+        $expect_desc = $result[3] > $result[1];
+        $this->assertEquals($expect_userId,1);
+        $this->assertEquals($expect_desc,true);
+    }
 
     public function test全てのメッセージを取得する()
     {
         $result = Model_Message::message_find_all();
         $expect = count($result);
-        $this->assertEquals($expect,2);
+        $this->assertEquals($expect,3);
     }
 
-    #public function testログインユーザへのメンション付きのメッセージを降順で取得する()
-    #{
-    #    $result = Model_Message::message_find_by_mention_desc_by_createdAt(1);
-    #    $expect_mention = $result[5]->mention;
-    #    $expect_desc = $result[6] > $result[5];
-    #    $this->assertEquals($expect_mention,1);
-    #    $this->assertEquals($expect_desc,true);
-    #}
+    public function testログインユーザへのメンション付きのメッセージを降順で取得する()
+    {
+        $result = Model_Message::message_find_by_mention_desc_by_createdAt(1);
+        $expect_mention = $result[2]->mention;
+        $expect_desc = $result[3] > $result[2];
+        $this->assertEquals($expect_mention,1);
+        $this->assertEquals($expect_desc,true);
+    }
 }
