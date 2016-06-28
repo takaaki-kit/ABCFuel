7fb909fca7e1279dbc068757061cfa90988ea97c
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-06-28 17:12:22 +0000

    userクラスのテストのリファクタ

--- fuel/app/classes/repository/user.php
+++ fuel/app/classes/repository/user.php
@@ -3,11 +3,6 @@
 
 class User
 {
-    static public function create_user_object()
-    {
-        return Model_User::forge();
-    }
-
     static public function find_by_screenName_and_password($screenName,$pass)
     {
         return  Model_User::find('first', array(
@@ -17,10 +12,4 @@ class User
             ),
         ));
     }
-
-    static public function save($user)
-    {
-        return $user->save();
-    }
-
 }
--- fuel/app/tests/repository/user.php
+++ fuel/app/tests/repository/user.php
@@ -19,7 +19,7 @@ class UserTest extends PHPUnit_Extensions_Database_TestCase
         $existUserScreenName = 'user1';
         $existUserPass = 'aho';
         $expect = User::find_by_screenName_and_password($existUserScreenName, $existUserPass);
-        $this->assertEquals(!empty($expect), true);
+        $this->assertTrue(!empty($expect));
     }
 
     public function test入力されたscreen_nameとpasswordが一致しなければ空のオブジェクトを返す()
@@ -27,22 +27,6 @@ class UserTest extends PHPUnit_Extensions_Database_TestCase
         $noExistUserScreenName = 'dummy';
         $noExistUserPass = 'dummy';
         $expect = User::find_by_screenName_and_password($noExistUserScreenName, $noExistUserPass);
-        $this->assertEquals(!empty($expect), false);
-    }
-
-    public function test空のuserオブジェクトを作成する()
-    {
-        $expect = get_class(User::create_user_object());
-        $this->assertEquals($expect, 'model_user');
-    }
-
-    public function testusersテーブルに値を保存する()
-    {
-        $user = User::create_user_object();
-        $user->screen_name = 'userID';
-        $user->name = 'userNAME';
-        $user->password = 'userPASS';
-        $expect = User::save($user);
-        $this->assertEquals($expect, true);
+        $this->assertTrue(empty($expect));
     }
 }
