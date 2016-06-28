3443bcdcc324fc41fc6b200b56b4618050fe9d90
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-06-28 14:42:55 +0000

    Loginクラスをプロダクトコードにも反映

--- fuel/app/classes/controller/login.php
+++ fuel/app/classes/controller/login.php
@@ -4,13 +4,15 @@ class controller_login extends Controller
 {
     public function get_index()
     {
-        return View::forge('login',['postParams' => User::create_user_object()]);
+        $user = new Login('','');
+        return View::forge('login',['postParams' => $user->get_user_model()]);
     }
+
     public function post_index()
     {
         $user = new Login(Input::param('screen_name'),Input::param('password'));
         if ($user->can_login() === false) {
-            $view = View::forge('login', ['postParams' => $postParams]);
+            $view = View::forge('login', ['postParams' => $user->get_user_model()]);
             $view->set_safe('error', 'IDかパスワードが違います');
 
             return $view;
