8f46cf1aca3dfc4df56f6b43ae7696020399a203
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-07-04 08:02:59 +0000

    login.phpのAuthクラスのインスタンスを受ける一時変数を$userから$authにリネーム

--- fuel/app/classes/controller/login.php
+++ fuel/app/classes/controller/login.php
@@ -4,21 +4,21 @@ class controller_login extends Controller
 {
     public function get_index()
     {
-        $user = new Auth();
+        $auth = new Auth();
 
         return View::forge('login', ['postParams' => $user->get_params()]);
     }
 
     public function post_index()
     {
-        $user = new Auth(Input::param('screen_name'), Input::param('password'));
-        if ($user->enable() === false) {
-            $view = View::forge('login', ['postParams' => $user->get_params()]);
+        $auth = new Auth(Input::param('screen_name'), Input::param('password'));
+        if ($auth->enable() === false) {
+            $view = View::forge('login', ['postParams' => $auth->get_params()]);
             $view->set_safe('error', 'IDかパスワードが違います');
 
             return $view;
         }
-        Session::set('id', $user->id());
+        Session::set('id', $auth->id());
         Response::redirect('/timeline');
     }
 }
